794d59676b15b252cf261839e3a2026e9a151295
mdozairq <mdozairq@gmail.com>
2021-09-22 13:04:08 +0530

    created userTable with CRUD Operation

--- src/App.js
+++ src/App.js
@@ -7,18 +7,249 @@ import axios from 'axios';
 
 function App() {
 
-  const [tableData, setTableData] = useState([]);
+  const [tableData, setTableData] = useState([
+    {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+    "lat": "-37.3159",
+    "lng": "81.1496"
+    }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+    }
+    },
+    {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+    "street": "Victor Plains",
+    "suite": "Suite 879",
+    "city": "Wisokyburgh",
+    "zipcode": "90566-7771",
+    "geo": {
+    "lat": "-43.9509",
+    "lng": "-34.4618"
+    }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+    "name": "Deckow-Crist",
+    "catchPhrase": "Proactive didactic contingency",
+    "bs": "synergize scalable supply-chains"
+    }
+    },
+    {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+    "street": "Douglas Extension",
+    "suite": "Suite 847",
+    "city": "McKenziehaven",
+    "zipcode": "59590-4157",
+    "geo": {
+    "lat": "-68.6102",
+    "lng": "-47.0653"
+    }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+    "name": "Romaguera-Jacobson",
+    "catchPhrase": "Face to face bifurcated interface",
+    "bs": "e-enable strategic applications"
+    }
+    },
+    {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+    "street": "Hoeger Mall",
+    "suite": "Apt. 692",
+    "city": "South Elvis",
+    "zipcode": "53919-4257",
+    "geo": {
+    "lat": "29.4572",
+    "lng": "-164.2990"
+    }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+    "name": "Robel-Corkery",
+    "catchPhrase": "Multi-tiered zero tolerance productivity",
+    "bs": "transition cutting-edge web services"
+    }
+    },
+    {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+    "address": {
+    "street": "Skiles Walks",
+    "suite": "Suite 351",
+    "city": "Roscoeview",
+    "zipcode": "33263",
+    "geo": {
+    "lat": "-31.8129",
+    "lng": "62.5342"
+    }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+    "name": "Keebler LLC",
+    "catchPhrase": "User-centric fault-tolerant solution",
+    "bs": "revolutionize end-to-end systems"
+    }
+    },
+    {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "email": "Karley_Dach@jasper.info",
+    "address": {
+    "street": "Norberto Crossing",
+    "suite": "Apt. 950",
+    "city": "South Christy",
+    "zipcode": "23505-1337",
+    "geo": {
+    "lat": "-71.4197",
+    "lng": "71.7478"
+    }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+    "name": "Considine-Lockman",
+    "catchPhrase": "Synchronised bottom-line interface",
+    "bs": "e-enable innovative applications"
+    }
+    },
+    {
+    "id": 7,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "email": "Telly.Hoeger@billy.biz",
+    "address": {
+    "street": "Rex Trail",
+    "suite": "Suite 280",
+    "city": "Howemouth",
+    "zipcode": "58804-1099",
+    "geo": {
+    "lat": "24.8918",
+    "lng": "21.8984"
+    }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+    "name": "Johns Group",
+    "catchPhrase": "Configurable multimedia task-force",
+    "bs": "generate enterprise e-tailers"
+    }
+    },
+    {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "email": "Sherwood@rosamond.me",
+    "address": {
+    "street": "Ellsworth Summit",
+    "suite": "Suite 729",
+    "city": "Aliyaview",
+    "zipcode": "45169",
+    "geo": {
+    "lat": "-14.3990",
+    "lng": "-120.7677"
+    }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+    "name": "Abernathy Group",
+    "catchPhrase": "Implemented secondary concept",
+    "bs": "e-enable extensible e-tailers"
+    }
+    },
+    {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "email": "Chaim_McDermott@dana.io",
+    "address": {
+    "street": "Dayna Park",
+    "suite": "Suite 449",
+    "city": "Bartholomebury",
+    "zipcode": "76495-3109",
+    "geo": {
+    "lat": "24.6463",
+    "lng": "-168.8889"
+    }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+    "name": "Yost and Sons",
+    "catchPhrase": "Switchable contextually-based project",
+    "bs": "aggregate real-time technologies"
+    }
+    },
+    {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "email": "Rey.Padberg@karina.biz",
+    "address": {
+    "street": "Kattie Turnpike",
+    "suite": "Suite 198",
+    "city": "Lebsackbury",
+    "zipcode": "31428-2261",
+    "geo": {
+    "lat": "-38.2386",
+    "lng": "57.2232"
+    }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+    "name": "Hoeger LLC",
+    "catchPhrase": "Centralized empowering task-force",
+    "bs": "target end-to-end models"
+    }
+    }
+    ]);
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(res => {
-        setTableData(...tableData, res.data);
-        //console.log(tableData);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }, [tableData])
+  // useEffect(() => {
+  //   axios.get("https://jsonplaceholder.typicode.com/users")
+  //     .then(res => {
+  //       setTableData(...tableData, res.data);
+  //       //console.log(tableData);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     })
+  // }, [tableData])
 
   const columns = [
     { title: "Serial", field: "id", emptyValue: () => <em>null</em> },
@@ -31,82 +262,48 @@ function App() {
 
   return (
     <div className="App">
-      <Container>
-        <h1>USER TABLE</h1>
-        <MaterialTable
-          components={{
-            Toolbar: props => (
-              <div style={{ backgroundColor: '#e8eaf5' }}>
-                <MTableToolbar {...props} />
-              </div>
-            )
-          }}
-          columns={columns}
-          options={{
-            sorting: true, search: true,
-            searchFieldAlignment: "right", searchAutoFocus: true, searchFieldVariant: "standard",
-            filtering: true, paging: true, pageSizeOptions: [5, 10, 20, 25, 50, 100], pageSize: 5,
-            paginationType: "stepped", showFirstLastPageButtons: false, paginationPosition: "both", exportButton: true,
-            exportAllData: true, exportFileName: "TableData", addRowPosition: "first", actionsColumnIndex: -1, selection: true,
-            showSelectAllCheckbox: false, showTextRowsSelected: false, selectionProps: rowData => ({
-              disabled: rowData.age == null,
-            }),
-            grouping: true, columnsButton: true,
-            rowStyle: (data, index) => index % 2 === 0 ? { background: "#f5f5f5" } : null,
-            headerStyle: { background: "#A7BBC7", color: "#fff" }
-          }}
-          data={tableData}
-          title="User Information"
+      <h1 align="center">React-App</h1>
+      <h4 align='center'>Crash Course on Material Table </h4>
 
-          editable={{
-            onRowAdd: (newRow) => new Promise((resolve, reject) => {
-              console.log(newRow)
-              axios.post("https://jsonplaceholder.typicode.com/user", newRow)
-                .then(res => {
-                  console.log(res.data);
-                  setTableData([...tableData, res.data]);
-                  resolve()
-                })
-                .catch(err => {
-                  console.log(err);
-                })
+      <MaterialTable columns={columns} data={tableData}
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            setTableData([...tableData, newRow])
 
-            }),
-            onRowUpdate: (newRow, oldRow) => new Promise((resolve, reject) => {
-              const updatedData = [...tableData]
-              axios.put(`https://jsonplaceholder.typicode.com/user/${oldRow.tableData.id}`, updatedData)
-                .then(res => {
-                  console.log(res.data);
-                  updatedData[oldRow.tableData.id] = newRow;
-                  setTableData(res.data);
-                  resolve()
-                })
-                .catch(err => {
-                  console.log(err);
-                })
+            setTimeout(() => resolve(), 500)
+          }),
+          onRowUpdate: (newRow, oldRow) => new Promise((resolve, reject) => {
+            const updatedData = [...tableData]
+            updatedData[oldRow.tableData.id] = newRow
+            setTableData(updatedData)
+            setTimeout(() => resolve(), 500)
+          }),
+          onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
+            const updatedData = [...tableData]
+            updatedData.splice(selectedRow.tableData.id, 1)
+            setTableData(updatedData)
+            setTimeout(() => resolve(), 1000)
 
-            }),
-            onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
-              console.log(selectedRow);
-              axios.delete(`https://jsonplaceholder.typicode.com/user/${selectedRow.tableData.id}`)
-                .then(res => {
-                  console.log(res.data);
-                  setTableData(res.data);
-                  resolve()
-                })
-                .catch(err => {
-                  console.log(err);
-                })
-              const updatedData = [...tableData];
-              updatedData.splice(selectedRow.tableData.id, 1)
-              setTableData(updatedData);
-              setTimeout(() => resolve(), 1000)
-            })
-          }}
+          })
+        }}
+        onSelectionChange={(selectedRows) => console.log(selectedRows)}
+        options={{
+          sorting: true, search: true,
+          searchFieldAlignment: "right", searchAutoFocus: true, searchFieldVariant: "standard",
+          filtering: true, paging: true, pageSizeOptions: [2, 5, 10, 20, 25, 50, 100], pageSize: 5,
+          paginationType: "stepped", showFirstLastPageButtons: false, paginationPosition: "both", exportButton: true,
+          exportAllData: true, exportFileName: "TableData", addRowPosition: "first", actionsColumnIndex: -1, selection: true,
+          showSelectAllCheckbox: false, showTextRowsSelected: false, selectionProps: rowData => ({
+            disabled: rowData.age == null,
+            // color:"primary"
+          }),
+          grouping: true, columnsButton: true,
+          rowStyle: (data, index) => index % 2 === 0 ? { background: "#f5f5f5" } : null,
+          headerStyle: { background: "#A7BBC7",color:"#fff"}
+        }}
+        title="Student Information"
         />
-      </Container>
     </div>
   );
 }
-
 export default App;
